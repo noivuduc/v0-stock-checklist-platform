2c4f2bd1aa95d2f16c830be6f8f4c21575039988
Noi <noivuduc@gmail.com>
2025-09-30 21:13:28 +0800

    Updated client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr"
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+   const db = 'https://dxjjrnklbasabyknbrpe.supabase.co';
+  const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ampybmtsYmFzYWJ5a25icnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTU1NzUsImV4cCI6MjA3NDczMTU3NX0.k4r-J-jl_11Nz7Jn4gWGrzNcFMyJ-Js_DOCnvaJhJzo'
+  const supabaseUrl = db || ''
+  const supabaseAnonKey = key || ''
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error(
